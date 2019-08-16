0b9c3ee484f7f29e7e22341d5b686aba3ad19f0b
mrdias87 <muka.dias@gmail.com>
2019-08-16 08:18:52 -0300

    OmniStack

---
+++ Backend/src/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const routes = require('./routes');
+
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+
+io.on('connection', socket => {
+    console.log('Nova conexão', socket.id);
+});
+
+mongoose.connect('mongodb+srv://reactdev:reactdev@cluster0-8qpsw.mongodb.net/omnistack8?retryWrites=true&w=majority',{
+    useNewUrlParser: true
+});
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+server.listen(3333);
---
+++ tindev/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+import Routes from './routes';
+
+export default function App()
+{
+  return(
+     <Routes />
+  );
+}
+
