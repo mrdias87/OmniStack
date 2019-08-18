20864acd51fe1b0b0847e0d7a72634dc68c21e00
mrdias87 <muka.dias@gmail.com>
2019-08-17 22:19:44 -0300

    Projeto completado

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -7,13 +7,23 @@ const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 
+const connectedUsers = {};
 
 io.on('connection', socket => {
-    console.log('Nova conexão', socket.id);
+    const { user } = socket.handshake.query;
+    
+    connectedUsers[user] = socket.id;
 });
 
 mongoose.connect('mongodb+srv://reactdev:reactdev@cluster0-8qpsw.mongodb.net/omnistack8?retryWrites=true&w=majority',{
     useNewUrlParser: true
+});
+
+app.use((req, res, next) => {
+    req.io = io;
+    req.connectedUsers = connectedUsers;
+
+    return next();
 });
 
 app.use(cors());
--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -1,14 +1,18 @@
 import React, {useEffect, useState} from 'react';
 import { Link } from 'react-router-dom';
+import io from 'socket.io-client';
 import './Main.css';
 import api from '../services/api';
 import logo from '../assets/logo.svg';
 import like from '../assets/like.svg';
 import dislike from '../assets/dislike.svg';
+import itsamatch from '../assets/itsamatch.svg';
 
 export default function Main({ match }){
    const [users, setUsers] = useState([]);
    
+    const [matchDev, setMatchDev] = useState(null);
+
     useEffect(() => {
        async function loadUsers(){
             const response = await api.get('/devs',{
@@ -22,6 +26,17 @@ export default function Main({ match }){
        loadUsers();
    }, [match.params.id]);
    
+   useEffect(() => {
+        const socket = io('http://localhost:3333', {
+            query: {user: match.params.id}
+        }); 
+
+        socket.on('match', dev => {
+            setMatchDev(dev);  
+        })
+   }, [match.params.id]);
+
+
    async function handleLike(id)
    {
         await api.post(`/devs/${id}/likes`, null, {
@@ -73,7 +88,16 @@ export default function Main({ match }){
                   </div>  
                 )}
                 
-            
+            {matchDev && (
+                <div className = "match-container" >
+                    <img src={itsamatch} alt="" />
+                    <img className="avatar" src={matchDev.avatar} alt="" />
+                    <strong>{matchDev.name}</strong>
+                    <p>{matchDev.bio}</p>
+
+                    <button type="button" onClick={() => setMatchDev(null)}>Fechar</button>
+                </div>
+            )}
         </div>
     )
 }
